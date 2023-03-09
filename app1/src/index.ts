d3465c22a6844aa903b0a709afb291857e3712be
Ankit Pandit(Consultant) <Ankit.Pandit@ril.com>
2023-03-09 21:02:13 +0530

    Deps made absolute

--- app1/src/index.ts
+++ app1/src/index.ts
@@ -1,4 +1,4 @@
-import { createBrowserHistory, Update } from "history";
+import { createBrowserHistory } from "history";
 
 import("./bootstrap").then(({ mount }) => {
   const localRoot = document.getElementById("app1-root");
